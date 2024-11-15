bf7b36f590171037bcd88557d28817c1ac24d04a
abhishek-475 <61498092+abhishek-475@users.noreply.github.com>
2024-11-15 07:49:11 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('db.json')
+const middleware = jsonServer.defaults()
+
+
+server.use(middleware)
+
+server.use(router)
+
+
+const PORT = process.env.PORT || 3001
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
